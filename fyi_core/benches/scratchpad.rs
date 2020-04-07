f9ff8b4c118fe8a803f33ba174860e0091e22e7e
Josh Stoik <josh@blobfolio.com>
2020-04-07 13:20:20 -0700

    misc: set to progress demo

--- fyi_core/benches/scratchpad.rs
+++ fyi_core/benches/scratchpad.rs
@@ -9,12 +9,14 @@ use criterion::{
 
 /// Bench.
 fn criterion_benchmark(c: &mut Criterion) {
-	use fyi_core::Progress;
-	use fyi_core::progress_arc as parc;
-	use std::thread;
-	use std::time::Duration;
+	use fyi_core::{
+		Progress,
+		arc::progress as parc,
+	};
 	use rayon::prelude::*;
 	use std::path::PathBuf;
+	use std::thread;
+	use std::time::Duration;
 
 	{
 		let bar = Progress::new("Funny thing is happening!", 300, 0);
@@ -37,15 +39,6 @@ fn criterion_benchmark(c: &mut Criterion) {
 		black_box(100),
 		black_box(0),
 	)));
-
-	/*let text: String = "Björk Guðmundsdóttir".to_string();
-	let colored: String = format!("{}", ansi_term::Colour::Blue.bold().paint(&text));
-
-	assert_eq!(&text.fyi_shorten(10), "Björk Guð…");
-	assert_eq!(&text.fyi_shorten(100), &text);
-
-	c.bench_function("shorten", |b| b.iter(|| text.fyi_shorten(black_box(10))));
-	c.bench_function("shorten", |b| b.iter(|| text.fyi_shorten(black_box(100))));*/
 }
 
 criterion_group!(benches, criterion_benchmark);
