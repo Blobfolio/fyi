30b61c4a95415c0e0ff6d6b2e2ac06fd88e5d95b
Josh Stoik <josh@blobfolio.com>
2020-05-26 22:50:28 -0700

    docs: improve documentation misc: remove pointless read/delete methods misc: assert more, if less, for stupid shit

--- fyi_witcher/src/traits/io.rs
+++ fyi_witcher/src/traits/io.rs
@@ -1,8 +1,12 @@
 /*!
 # FYI Witcher Traits: Path I/O
 
-This trait adds a handful of read/write/copy/move-type methods to `Path`
-objects.
+This trait adds a few useful I/O methods to `Path`:
+* `witch_copy()` Copy a file byte-for-byte to a new location.
+* `witch_copy_tmp()` The same as above, but a tempfile is used (and returned) instead.
+* `witch_move()` Move a file, byte for byte, to a new location.
+* `witch_reference_from()` Copy ownership and permissions from another `Path`.
+* `witch_write()` Write or rewrite data to a file, using a tempfile in the middle for safety.
 */
 
 use crate::Result;
@@ -18,7 +22,7 @@ use tempfile::NamedTempFile;
 
 
 
-/// Format/Conversion/Mutation Helpers!
+/// Witch I/O
 pub trait WitchIO {
 	/// Byte for Byte Copy.
 	fn witch_copy<P> (&self, to: P) -> Result<()>
@@ -27,15 +31,9 @@ pub trait WitchIO {
 	/// Copy To Temporary Location.
 	fn witch_copy_tmp(&self, suffix: Option<String>) -> Result<NamedTempFile>;
 
-	/// Delete.
-	fn witch_delete(&self) -> Result<()>;
-
 	/// Move.
 	fn witch_move<P> (&self, to: P) -> Result<()>
 	where P: AsRef<Path>;
-
-	/// Read Bytes.
-	fn witch_read(&self) -> Result<Vec<u8>>;
 
 	/// Chown/Chmod to Reference.
 	fn witch_reference_from<P> (&self, from: P) -> Result<(bool, bool)>
@@ -46,142 +44,146 @@ pub trait WitchIO {
 }
 
 impl WitchIO for Path {
-	/// Byte for Byte Copy.
+	/// Copy File.
+	///
+	/// Copy the byte content of a file to a new location, using a tempfile in
+	/// the middle for safety.
+	///
+	/// It is worth noting that this does *not* copy ownership or
+	/// permissions to the new target. If that file already existed, it will
+	/// keep its original attributes, otherwise the system will set whatever
+	/// it deems appropriate.
+	///
+	/// Panics if `self` is not a file or `to` is a directory.
 	fn witch_copy<P> (&self, to: P) -> Result<()>
 	where P: AsRef<Path> {
-		if self.is_file() {
-			let to = to.as_ref().to_path_buf();
-			to.witch_write(&fs::read(&self).map_err(|e| e.to_string())?)?;
+		assert!(
+			self.exists() &&
+			! self.is_dir() &&
+			! to.as_ref().is_dir()
+		);
 
-			Ok(())
-		}
-		else {
-			Err(format!(
-				"Unable to copy {:?} to {:?}.",
-				self,
-				to.as_ref(),
-			))
-		}
-	}
-
-	/// Copy To Temporary Location.
-	fn witch_copy_tmp(&self, suffix: Option<String>) -> Result<NamedTempFile> {
-		let meta = self.metadata().map_err(|e| e.to_string())?;
-		if meta.is_file() {
-			let parent = self.parent()
-				.ok_or_else(|| format!(
-					"Unable to copy {:?} to tmpfile.",
-					self,
-				))?;
-
-
-			// Allocate a tempfile.
-			let target = match suffix {
-				Some(x) => tempfile::Builder::new()
-					.suffix(OsStr::new(x.as_str()))
-					.tempfile_in(parent).map_err(|e| e.to_string())?,
-				None => NamedTempFile::new_in(parent).map_err(|e| e.to_string())?,
-			};
-
-			// Copy references.
-			target.path().witch_reference_from(&self)?;
-
-			// Write data.
-			let mut file = target.as_file();
-			let data: Vec<u8> = fs::read(&self).map_err(|e| e.to_string())?;
-			file.write_all(&data).map_err(|e| e.to_string())?;
-			file.flush().unwrap();
-
-			Ok(target)
-		}
-		else {
-			Err(format!(
-				"Unable to copy {:?} to tmpfile.",
-				self,
-			))
-		}
-	}
-
-	/// Delete.
-	fn witch_delete(&self) -> Result<()> {
-		if self.is_file() {
-			fs::remove_file(&self).map_err(|e| e.to_string())?;
-			Ok(())
-		}
-		else if self.exists() {
-			Err(format!(
-				"Unable to delete {:?}.",
-				self,
-			))
-		}
-		else {
-			Ok(())
-		}
-	}
-
-	/// Move.
-	fn witch_move<P> (&self, to: P) -> Result<()>
-	where P: AsRef<Path> {
-		self.witch_copy(&to)?;
-		self.witch_delete()?;
+		let to = to.as_ref().to_path_buf();
+		to.witch_write(&fs::read(&self).map_err(|e| e.to_string())?)?;
 
 		Ok(())
 	}
 
-	/// Read Bytes.
-	fn witch_read(&self) -> Result<Vec<u8>> {
-		if self.is_file() {
-			Ok(fs::read(&self).map_err(|e| e.to_string())?)
-		}
-		else {
-			Err(format!(
-				"Unable to read {:?}.",
+	/// Copy To Temporary Location.
+	///
+	/// This behaves similarly to `witch_copy()`, except the destination *is*
+	/// a tempfile, which gets returned at the end so it can be persisted or
+	/// forgotten accordingly.
+	///
+	/// Unlike `witch_copy()` or `witch_move()`, this method *does* attempt to
+	/// copy ownership and permission to the tempfile. (Its path is always
+	/// unoccupied, so might as well choose its default.)
+	///
+	/// Panics if `self` is not a file.
+	fn witch_copy_tmp(&self, suffix: Option<String>) -> Result<NamedTempFile> {
+		assert!(self.exists() && ! self.is_dir());
+
+		let parent = self.parent()
+			.ok_or_else(|| format!(
+				"Unable to copy {:?} to tmpfile.",
 				self,
-			))
-		}
+			))?;
+
+		// Allocate a tempfile.
+		let target = match suffix {
+			Some(x) => tempfile::Builder::new()
+				.suffix(OsStr::new(x.as_str()))
+				.tempfile_in(parent).map_err(|e| e.to_string())?,
+			None => NamedTempFile::new_in(parent).map_err(|e| e.to_string())?,
+		};
+
+		// Copy references.
+		target.path().witch_reference_from(&self)?;
+
+		// Write data.
+		let mut file = target.as_file();
+		file.write_all(&fs::read(&self).map_err(|e| e.to_string())?)
+			.map_err(|e| e.to_string())?;
+		file.flush().map_err(|e| e.to_string())?;
+
+		Ok(target)
 	}
 
-	/// Chown/Chmod to Reference.
+	/// Move File.
+	///
+	/// Move the byte content of a file to new location, using a tempfile in
+	/// the middle for safety.
+	///
+	/// This is exactly like `witch_copy()`, except `self` is deleted
+	/// afterwards.
+	///
+	/// Panics if `self` is not a file, or `to` is a directory.
+	fn witch_move<P> (&self, to: P) -> Result<()>
+	where P: AsRef<Path> {
+		assert!(
+			self.exists() &&
+			! self.is_dir() &&
+			! to.as_ref().is_dir()
+		);
+
+		self.witch_copy(&to)?;
+		fs::remove_file(&self).map_err(|e| e.to_string())?;
+
+		Ok(())
+	}
+
+	/// Chown/Chmod From Reference.
+	///
+	/// Copy ownership and permissions from `from` to `self`, if possible.
+	///
+	/// On *nix systems, programs can only do what the users running them are
+	/// allowed to do, so depending on the particulars, these operations may
+	/// work only partially or fail entirely.
+	///
+	/// Unless a hard error is hit while trying to read the metadata of the
+	/// reference file, the method will return separate `boolean` values for
+	/// each part, permissions first, then ownership.
+	///
+	/// Panics if either path is not a file.
 	fn witch_reference_from<P> (&self, from: P) -> Result<(bool, bool)>
 	where P: AsRef<Path> {
+		assert!(
+			self.exists() &&
+			from.as_ref().exists() &&
+			! self.is_dir() &&
+			! from.as_ref().is_dir()
+		);
+
 		let meta = from.as_ref().metadata().map_err(|e| e.to_string())?;
-		if meta.is_file() && self.is_file() {
-			Ok((
-				fs::set_permissions(&self, meta.permissions()).is_ok(),
-				unistd::chown(
-					self,
-					Some(Uid::from_raw(meta.uid())),
-					Some(Gid::from_raw(meta.gid()))
-				).is_ok()
-			))
-		}
-		else {
-			Err(format!(
-				"Unable to set owner/perms for {:?} using {:?}.",
+		Ok((
+			fs::set_permissions(&self, meta.permissions()).is_ok(),
+			unistd::chown(
 				self,
-				from.as_ref(),
-			))
-		}
+				Some(Uid::from_raw(meta.uid())),
+				Some(Gid::from_raw(meta.gid()))
+			).is_ok()
+		))
 	}
 
-	/// Write Bytes.
+	/// Write Bytes to File.
+	///
+	/// This is a small wrapper around `tempfile_fast`, which writes data to a
+	/// tempfile before committing them to the chosen destination.
+	///
+	/// If `self` doesn't exist, it will be created.
+	///
+	/// Panics if `self` is a directory.
 	fn witch_write(&self, data: &[u8]) -> Result<()> {
-		if self.is_dir() {
-			Err(format!(
-				"Unable to write to {:?}.",
-				self,
-			))
-		}
-		else {
-			let mut tmp = tempfile_fast::Sponge::new_for(&self)
-				.map_err(|e| e.to_string())?;
-			tmp.write_all(data)
-				.map_err(|e| e.to_string())?;
-			tmp.commit()
-				.map_err(|e| e.to_string())?;
+		assert!(! self.is_dir());
 
-			Ok(())
-		}
+		let mut tmp = tempfile_fast::Sponge::new_for(&self)
+			.map_err(|e| e.to_string())?;
+		tmp.write_all(data)
+			.map_err(|e| e.to_string())?;
+		tmp.commit()
+			.map_err(|e| e.to_string())?;
+
+		Ok(())
 	}
 }
 
@@ -203,7 +205,7 @@ mod tests {
 
 		// Make sure the destination is empty before starting.
 		if to.is_file() {
-			to.witch_delete().unwrap();
+			fs::remove_file(&to).unwrap();
 		}
 
 		assert!(from.is_file());
@@ -214,7 +216,7 @@ mod tests {
 		assert_eq!(file_size(&from), file_size(&to));
 
 		// And remove it.
-		to.witch_delete().unwrap();
+		fs::remove_file(&to).unwrap();
 	}
 
 	#[test]
@@ -249,8 +251,6 @@ mod tests {
 		}
 	}
 
-	// Note: witch_delete() is covered multiple times by other tests.
-
 	#[test]
 	fn witch_move() {
 		let src: PathBuf = PathBuf::from("tests/assets/file.txt");
@@ -262,7 +262,7 @@ mod tests {
 
 		// Make sure the destination is empty before starting.
 		if to.is_file() {
-			to.witch_delete().unwrap();
+			fs::remove_file(&to).unwrap();
 		}
 
 		assert!(from.is_file());
@@ -276,18 +276,7 @@ mod tests {
 		assert_eq!(file_size(&src), file_size(&to));
 
 		// And remove it.
-		to.witch_delete().unwrap();
-	}
-
-	#[test]
-	fn witch_read() {
-		let path: PathBuf = PathBuf::from("tests/assets/file.txt");
-		assert!(path.is_file());
-
-		let data: Vec<u8> = path.witch_read().unwrap();
-		let human: String = String::from_utf8(data).unwrap();
-
-		assert_eq!(&human, "This is just a text file.\n");
+		fs::remove_file(&to).unwrap();
 	}
 
 	#[test]
@@ -303,7 +292,7 @@ mod tests {
 		// Write it.
 		path.witch_write(data.as_bytes()).unwrap();
 
-		let data2: Vec<u8> = path.witch_read().unwrap();
+		let data2: Vec<u8> = fs::read(&path).unwrap();
 		let human: String = String::from_utf8(data2).unwrap();
 
 		assert_eq!(data, human);
