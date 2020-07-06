75d0d40d71bb15b5b2e5c404926b6eebf0b10009
Josh Stoik <josh@blobfolio.com>
2020-07-05 22:20:07 -0700

    misc: move handling into Enum

--- fyi/src/main.rs
+++ fyi/src/main.rs
@@ -37,12 +37,170 @@ use std::{
 
 
 
-/// Short Circuit: If a help switch is included on a subcommand, drop
-/// everything and show the info.
-macro_rules! get_help {
-	($com:expr, $opts:ident) => {
-		if $opts.pluck_help() { return _help(Some($com)); }
-	};
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+enum Command {
+	Blank,
+	Confirm,
+	Crunched,
+	Debug,
+	Done,
+	Error,
+	Help,
+	Info,
+	Notice,
+	Print,
+	Success,
+	Task,
+	Warning,
+}
+
+impl From<&str> for Command {
+	fn from(raw: &str) -> Self {
+		match raw {
+			"blank" => Self::Blank,
+			"confirm" => Self::Confirm,
+			"crunched" | "prompt" => Self::Crunched,
+			"debug" => Self::Debug,
+			"done" => Self::Done,
+			"error" => Self::Error,
+			"help" => Self::Help,
+			"info" => Self::Info,
+			"notice" => Self::Notice,
+			"print" => Self::Print,
+			"success" => Self::Success,
+			"task" => Self::Task,
+			"warning" => Self::Warning,
+			_ => {
+				ArgList::die("Invalid subcommand.");
+				unreachable!();
+			},
+		}
+	}
+}
+
+impl Command {
+	/// As String.
+	pub fn as_str(self) -> &'static str {
+		match self {
+			Self::Blank => "Blank",
+			Self::Confirm => "Confirm",
+			Self::Crunched => "Crunched",
+			Self::Debug => "Debug",
+			Self::Done => "Done",
+			Self::Error => "Error",
+			Self::Help => "Help",
+			Self::Info => "Info",
+			Self::Notice => "Notice",
+			Self::Print => "Print",
+			Self::Success => "Success",
+			Self::Task => "Task",
+			Self::Warning => "Warning",
+		}
+	}
+
+	/// As `Msg`.
+	pub fn as_msg(self, args: &mut ArgList) -> Msg {
+		match self {
+			Self::Print => {
+				let color: u8 = 255.min(args.pluck_opt_usize(|x| x == "-c" || x == "--prefix-color").unwrap_or(199)) as u8;
+				let prefix = args.pluck_opt(|x| x == "-p" || x == "--prefix").unwrap_or_default();
+				Msg::new(prefix, color, args.expect_arg())
+			},
+			Self::Confirm => Msg::confirm(args.expect_arg()),
+			Self::Crunched => Msg::crunched(args.expect_arg()),
+			Self::Debug => Msg::debug(args.expect_arg()),
+			Self::Done => Msg::done(args.expect_arg()),
+			Self::Error => Msg::error(args.expect_arg()),
+			Self::Info => Msg::info(args.expect_arg()),
+			Self::Notice => Msg::notice(args.expect_arg()),
+			Self::Success => Msg::success(args.expect_arg()),
+			Self::Task => Msg::task(args.expect_arg()),
+			Self::Warning => Msg::warning(args.expect_arg()),
+			_ => Msg::default(),
+		}
+	}
+
+	/// Execute.
+	pub fn exec(self, args: &mut ArgList) {
+		if args.pluck_help() {
+			self.help();
+			return;
+		}
+
+		match self {
+			Self::Blank => {
+				let count: usize = match args.pluck_opt_usize(|x| x == "-c" || x == "--count") {
+					Some(c) => 10.min(1.max(c)),
+					None => 1,
+				};
+
+				if args.pluck_switch(match_stderr) {
+					io::stderr().write_all(&[10].repeat(count)).unwrap();
+				}
+				else {
+					io::stdout().write_all(&[10].repeat(count)).unwrap();
+				}
+			},
+			Self::Confirm => {
+				let indent = args.pluck_switch(match_indent);
+				let timestamp = args.pluck_switch(match_timestamp);
+				let mut msg = self.as_msg(args);
+
+				if indent {
+					msg.set_indent(1);
+				}
+
+				if timestamp {
+					msg.set_timestamp();
+				}
+
+				if ! msg.prompt() {
+					process::exit(1);
+				}
+			},
+			_ => {
+				let indent = args.pluck_switch(match_indent);
+				let timestamp = args.pluck_switch(match_timestamp);
+				let stderr = args.pluck_switch(match_stderr);
+				let exit: u8 = args.pluck_opt_usize(match_exit).unwrap_or(0) as u8;
+				let mut msg = self.as_msg(args);
+
+				if indent {
+					msg.set_indent(1);
+				}
+
+				if timestamp {
+					msg.set_timestamp();
+				}
+
+				// Print it to `Stderr`.
+				if stderr { msg.eprintln(); }
+				// Print it to `Stdout`.
+				else { msg.println(); }
+
+				// We might have a custom exit code.
+				if 0 != exit {
+					process::exit(i32::from(exit));
+				}
+			},
+		}
+	}
+
+	#[cold]
+	/// Help.
+	pub fn help(self) {
+		match self {
+			Self::Blank => _help(include_str!("../help/blank.txt")),
+			Self::Confirm => _help(include_str!("../help/confirm.txt")),
+			Self::Print => _help(include_str!("../help/print.txt")),
+			Self::Help => _help(include_str!("../help/help.txt")),
+			_ => _help(&format!(
+				include_str!("../help/generic.txt"),
+				self.as_str(),
+				self.as_str().to_lowercase(),
+			)),
+		}
+	}
 }
 
 
@@ -54,207 +212,51 @@ fn main() {
 	// The app might be called with version or help flags instead of a command.
 	match args.peek().unwrap() {
 		"-V" | "--version" => _version(),
-		"-h" | "--help" | "help" => _help(None),
+		"-h" | "--help" | "help" => Command::Help.help(),
 		// Otherwise just go off into the appropriate subcommand action.
-		_ => match args.expect_command().as_str() {
-			"blank" => _blank(&mut args),
-			"confirm" | "prompt" => _confirm(&mut args),
-			"print" => _custom(&mut args),
-			other => _builtin(other, &mut args),
-		},
+		_ => Command::from(args.expect_command().as_str()).exec(&mut args),
 	}
 }
 
 #[allow(clippy::ptr_arg)]
 /// Match: Exit Code
-fn match_exit(txt: &String) -> bool {
-	txt == "-e" || txt == "--exit"
-}
+fn match_exit(txt: &String) -> bool { txt == "-e" || txt == "--exit" }
 
 #[allow(clippy::ptr_arg)]
 /// Match: Indentation
-fn match_indent(txt: &String) -> bool {
-	txt != "-i" && txt != "--indent"
-}
+fn match_indent(txt: &String) -> bool { txt != "-i" && txt != "--indent" }
 
 #[allow(clippy::ptr_arg)]
 /// Match: Stderr
-fn match_stderr(txt: &String) -> bool {
-	txt != "--stderr"
-}
+fn match_stderr(txt: &String) -> bool { txt != "--stderr" }
 
 #[allow(clippy::ptr_arg)]
 /// Match: Timestamp
-fn match_timestamp(txt: &String) -> bool {
-	txt != "-t" && txt != "--timestamp"
-}
+fn match_timestamp(txt: &String) -> bool { txt != "-t" && txt != "--timestamp" }
 
 #[cold]
-/// Handle Blank
-fn _blank(opts: &mut ArgList) {
-	get_help!("blank", opts);
-
-	let count: usize = match opts.pluck_opt_usize(|x| x == "-c" || x == "--count") {
-		Some(c) => 10.min(1.max(c)),
-		None => 1,
-	};
-
-	if opts.pluck_switch(match_stderr) {
-		io::stderr().write_all(&[10].repeat(count)).unwrap();
-	}
-	else {
-		io::stdout().write_all(&[10].repeat(count)).unwrap();
-	}
-}
-
-/// Handle Built-In Prefix.
-fn _builtin(com: &str, opts: &mut ArgList) {
-	get_help!(com, opts);
-
-	// Pull switches.
-	let indent = opts.pluck_switch(match_indent);
-	let timestamp = opts.pluck_switch(match_timestamp);
-	let stderr = opts.pluck_switch(match_stderr);
-
-	// Exit is an option.
-	let exit: u8 = opts.pluck_opt_usize(match_exit).unwrap_or(0) as u8;
-
-	// And finally the message bit!
-	_msg(
-		match com {
-			"crunched" => Msg::crunched(opts.expect_arg()),
-			"debug" => Msg::debug(opts.expect_arg()),
-			"done" => Msg::done(opts.expect_arg()),
-			"error" => Msg::error(opts.expect_arg()),
-			"info" => Msg::info(opts.expect_arg()),
-			"notice" => Msg::notice(opts.expect_arg()),
-			"success" => Msg::success(opts.expect_arg()),
-			"task" => Msg::task(opts.expect_arg()),
-			"warning" => Msg::warning(opts.expect_arg()),
-			_ => {
-				ArgList::die("Invalid subcommand.");
-				unreachable!();
-			},
-		},
-		indent,
-		timestamp,
-		stderr,
-		i32::from(exit)
-	);
-}
-
-/// Handle Confirmation.
-fn _confirm(opts: &mut ArgList) {
-	get_help!("confirm", opts);
-
-	let indent = opts.pluck_switch(match_indent);
-	let timestamp = opts.pluck_switch(match_timestamp);
-
-	let mut msg = Msg::confirm(opts.expect_arg());
-
-	if indent {
-		msg.set_indent(1);
-	}
-
-	if timestamp {
-		msg.set_timestamp();
-	}
-
-	if ! msg.prompt() {
-		process::exit(1);
-	}
-}
-
-/// Custom Prefix.
-fn _custom(opts: &mut ArgList) {
-	get_help!("print", opts);
-
-	// Pull switches.
-	let indent = opts.pluck_switch(match_indent);
-	let timestamp = opts.pluck_switch(match_timestamp);
-	let stderr = opts.pluck_switch(match_stderr);
-
-	// Pull the options.
-	let exit: u8 = opts.pluck_opt_usize(match_exit).unwrap_or(0) as u8;
-	let color: u8 = 255.min(opts.pluck_opt_usize(|x| x == "-c" || x == "--prefix-color").unwrap_or(199)) as u8;
-	let prefix = opts.pluck_opt(|x| x == "-p" || x == "--prefix").unwrap_or_default();
-
-	// And finally the message bit!
-	_msg(
-		Msg::new(prefix, color, opts.expect_arg()),
-		indent,
-		timestamp,
-		stderr,
-		i32::from(exit)
-	)
-}
-
-#[cold]
-/// Print help and exit.
-fn _help(com: Option<&str>) {
-	match com.unwrap_or_default() {
-		"blank" => _helpful(include_str!("../help/blank.txt")),
-		"confirm" | "prompt" => _helpful(include_str!("../help/confirm.txt")),
-		"crunched" => _help_generic("crunched", "Crunched"),
-		"debug" => _help_generic("debug", "Debug"),
-		"done" => _help_generic("done", "Done"),
-		"error" => _help_generic("error", "Error"),
-		"info" => _help_generic("info", "Info"),
-		"notice" => _help_generic("notice", "Notice"),
-		"print" => _helpful(include_str!("../help/print.txt")),
-		"success" => _help_generic("success", "Success"),
-		"task" => _help_generic("task", "Task"),
-		"warning" => _help_generic("warning", "Warning"),
-		_ => _helpful(include_str!("../help/help.txt"))
-	}
-}
-
-#[cold]
-/// Generic Subcommand Help
-///
-/// Most of the built-ins work exactly the same way.
-fn _help_generic(com: &str, name: &str) {
-	_helpful(&format!(include_str!("../help/generic.txt"), name, com));
-}
-
-#[cold]
-/// Print full help.
-fn _helpful(help: &str) {
-	io::stdout().write_all(format!(
-		"FYI {}\n{}\n\n{}\n",
-		env!("CARGO_PKG_VERSION"),
-		env!("CARGO_PKG_DESCRIPTION"),
-		help,
-	).as_bytes()).unwrap();
-}
-
-/// Print Message.
-fn _msg(mut msg: Msg, indent: bool, timestamp: bool, stderr: bool, exit: i32) {
-	if indent {
-		msg.set_indent(1);
-	}
-
-	if timestamp {
-		msg.set_timestamp();
-	}
-
-	// Print it to `Stderr`.
-	if stderr {
-		msg.eprintln();
-	}
-	// Print it to `Stdout`.
-	else {
-		msg.println();
-	}
-
-	// We might have a custom exit code.
-	if 0 != exit {
-		process::exit(exit);
-	}
+/// Print Help.
+fn _help(txt: &str) {
+	io::stdout().write_all({
+		let mut s = String::with_capacity(1024);
+		s.push_str("FYI ");
+		s.push_str(env!("CARGO_PKG_VERSION"));
+		s.push('\n');
+		s.push_str(env!("CARGO_PKG_DESCRIPTION"));
+		s.push('\n');
+		s.push('\n');
+		s.push_str(txt);
+		s.push('\n');
+		s
+	}.as_bytes()).unwrap();
 }
 
 #[cold]
 /// Print version and exit.
 fn _version() {
-	io::stdout().write_all(["FYI", env!("CARGO_PKG_VERSION")].join(" ").as_bytes()).unwrap();
+	io::stdout().write_all({
+		let mut s = String::from("FYI ");
+		s.push_str(env!("CARGO_PKG_VERSION"));
+		s
+	}.as_bytes()).unwrap();
 }
