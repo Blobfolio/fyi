6e9aee4f895d209f0ded6b91d473a6b0222ce8ba
Josh Stoik <josh@blobfolio.com>
2020-08-18 00:20:08 -0700

    cleanup: compact help methods

--- fyi/src/main.rs
+++ fyi/src/main.rs
@@ -93,84 +93,15 @@ fn blank(args: &mut Argue) {
 	}
 }
 
+#[cfg(not(feature = "man"))]
 /// Help Page.
 ///
 /// Print the appropriate help screen given the call details. Most of the sub-
 /// commands work the same way, but a few have their own distinct messages.
 fn helper(cmd: Option<&str>) {
-	match cmd {
-		Some("blank") => _help(include_bytes!("../help/blank.txt")),
-		Some("print") => _help(include_bytes!("../help/print.txt")),
-		Some("confirm") | Some("prompt") => _help(include_bytes!("../help/confirm.txt")),
-		Some(x) if MsgKind::from(x) != MsgKind::None => {
-			_help(format!(
-				include_str!("../help/generic.txt"),
-				x,
-				MsgKind::from(x).into_msg("Hello World").as_str(),
-				x.to_lowercase(),
-			).as_bytes())
-		},
-		_ => _help(include_bytes!("../help/help.txt")),
-	}
-}
-
-/// Print Message!
-///
-/// Almost all roads lead to this method, which crunches the CLI args and
-/// prints an appropriately formatted message.
-fn message(kind: MsgKind, args: &mut Argue) {
-	// Exit code.
-	let exit: i32 = args.option2("-e", "--exit")
-		.map_or(0, |x| x.parse::<i32>().unwrap_or(0));
-
-	// Basic flags.
-	let mut flags: u8 = 0;
-	if args.switch2("-i", "--indent") { flags |= FLAG_INDENT; }
-	if args.switch2("-t", "--timestamp") { flags |= FLAG_TIMESTAMP; }
-
-	// Let's build the message!
-	let msg = kind.into_msg(args.take_arg()).with_flags(flags);
-
-	// It's a prompt!
-	if MsgKind::Confirm == kind {
-		if ! msg.prompt() {
-			process::exit(1);
-		}
-
-		return;
-	}
-	// Print to `Stderr`.
-	else if args.switch("--stderr") { msg.eprintln(); }
-	// Print to `Stdout`.
-	else { msg.println(); }
-
-	// Special exit?
-	if 0 != exit {
-		process::exit(exit);
-	}
-}
-
-/// Print Version.
-fn versioner() {
-	let writer = std::io::stdout();
-	let mut handle = writer.lock();
-
-	handle.write_all(b"FYI ").unwrap();
-	handle.write_all(env!("CARGO_PKG_VERSION").as_bytes()).unwrap();
-	handle.write_all(b"\n").unwrap();
-
-	handle.flush().unwrap();
-}
-
-#[cfg(not(feature = "man"))]
-#[cold]
-/// Print Help.
-///
-/// This actually prints the help screen. This version of the method is used in
-/// the compiled binary.
-fn _help(txt: &[u8]) {
-	io::stdout().write_fmt(format_args!(
-		r#"
+	Msg::from(
+		format!(
+			r#"
                       ;\
                      |' \
   _                  ; : ;
@@ -194,31 +125,76 @@ fn _help(txt: &[u8]) {
             '`      ,'
                  ,-'
 
-{}
-"#,
-		"\x1b[38;5;199mFYI\x1b[0;38;5;69m v",
-		env!("CARGO_PKG_VERSION"),
-		"\x1b[0m",
-		unsafe { std::str::from_utf8_unchecked(txt) }
-	)).unwrap();
+{}"#,
+			"\x1b[38;5;199mFYI\x1b[0;38;5;69m v",
+			env!("CARGO_PKG_VERSION"),
+			"\x1b[0m",
+			match cmd {
+				Some("blank") => include_str!("../help/blank.txt").to_string(),
+				Some("print") => include_str!("../help/print.txt").to_string(),
+				Some("confirm") | Some("prompt") => include_str!("../help/confirm.txt").to_string(),
+				Some(x) if MsgKind::from(x) != MsgKind::None => format!(
+					include_str!("../help/generic.txt"),
+					x,
+					MsgKind::from(x).into_msg("Hello World").as_str(),
+					x.to_lowercase(),
+				),
+				_ => include_str!("../help/help.txt").to_string(),
+			}
+		)
+	).println();
 }
 
 #[cfg(feature = "man")]
 #[cold]
-/// Print Help.
+/// Help Page.
 ///
-/// This actually prints the help screen. This version of the method is used
-/// during build to give `help2man` something to work with.
-fn _help(txt: &[u8]) {
-	versioner();
+/// This is a boring version of the help screen used during build to make it
+/// easier for `help2man` to parse.
+fn helper(_: Option<&str>) {
+	Msg::from([
+		b"FYI ",
+		env!("CARGO_PKG_VERSION").as_bytes(),
+		b"\n",
+		env!("CARGO_PKG_DESCRIPTION").as_bytes(),
+		b"\n\n",
+		include_bytes!("../help/generic.txt"),
+	].concat())
+		.println();
+}
 
-	let writer = std::io::stdout();
-	let mut handle = writer.lock();
+/// Print Message!
+///
+/// Almost all roads lead to this method, which crunches the CLI args and
+/// prints an appropriately formatted message.
+fn message(kind: MsgKind, args: &mut Argue) {
+	// Exit code.
+	let exit: i32 = args.option2("-e", "--exit")
+		.map_or(0, |x| x.parse::<i32>().unwrap_or(0));
 
-	handle.write_all(env!("CARGO_PKG_DESCRIPTION").as_bytes()).unwrap();
-	handle.write_all(b"\n\n").unwrap();
-	handle.write_all(txt).unwrap();
-	handle.write_all(b"\n").unwrap();
+	// Basic flags.
+	let mut flags: u8 = 0;
+	if args.switch2("-i", "--indent") { flags |= FLAG_INDENT; }
+	if args.switch2("-t", "--timestamp") { flags |= FLAG_TIMESTAMP; }
 
-	handle.flush().unwrap();
+	// Let's build the message!
+	let msg = kind.into_msg(args.take_arg()).with_flags(flags);
+
+	// It's a prompt!
+	if MsgKind::Confirm == kind {
+		if ! msg.prompt() {
+			std::process::exit(1);
+		}
+
+		return;
+	}
+	// Print to `Stderr`.
+	else if args.switch("--stderr") { msg.eprintln(); }
+	// Print to `Stdout`.
+	else { msg.println(); }
+
+	// Special exit?
+	if 0 != exit {
+		std::process::exit(exit);
+	}
 }
