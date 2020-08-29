55cf1f0df74af2d158a5e335f0d53dcf5057b236
Josh Stoik <josh@blobfolio.com>
2020-08-28 21:48:40 -0700

    new: bench ends_with_ignore_ascii_case()

--- fyi_witcher/benches/fw_utility.rs
+++ fyi_witcher/benches/fw_utility.rs
@@ -12,6 +12,31 @@ use fyi_witcher::utility;
 
 
 
+fn ends_with_ignore_ascii_case(c: &mut Criterion) {
+	let mut group = c.benchmark_group("fyi_witcher::utility");
+
+	for paths in [
+		b"/home/user/Pictures/file01.jpg",
+		b"/home/user/Pictures/file01.JPG",
+		b"/home/user/Pictures/file01.png",
+	].iter() {
+		group.bench_with_input(
+			BenchmarkId::from_parameter(format!(
+				"ends_with_ignore_ascii_case({}, .jpg)",
+				unsafe { std::str::from_utf8_unchecked(&paths[..]) },
+			)),
+			paths,
+			|b, &paths| {
+				b.iter(||
+					utility::ends_with_ignore_ascii_case(paths, b".jpg")
+				);
+			}
+		);
+	}
+
+	group.finish();
+}
+
 fn secs_chunks(c: &mut Criterion) {
 	let mut group = c.benchmark_group("fyi_witcher::utility");
 
@@ -38,5 +63,6 @@ fn secs_chunks(c: &mut Criterion) {
 criterion_group!(
 	benches,
 	secs_chunks,
+	ends_with_ignore_ascii_case,
 );
 criterion_main!(benches);
