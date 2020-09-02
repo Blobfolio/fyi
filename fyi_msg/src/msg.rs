b87b1df9268a5cd162cc21240bc636df8f2777c0
Josh Stoik <josh@blobfolio.com>
2020-09-01 18:13:11 -0700

    misc: let toc.replace handle equivalence checks

--- fyi_msg/src/msg.rs
+++ fyi_msg/src/msg.rs
@@ -565,21 +565,18 @@ impl Msg {
 	/// msg.set_indent(2); // "        Hello World."
 	/// ```
 	pub fn set_indent(&mut self, indent: u8) {
-		let indent: u8 = 4.min(indent);
-		if indent != self.toc.len(PART_INDENT) as u8 {
-			self.toc.replace(
-				&mut self.buf,
-				PART_INDENT,
-				match indent {
-					0 => b"",
-					1 => b"    ",
-					2 => b"        ",
-					3 => b"            ",
-					4 => b"                ",
-					_ => unreachable!(),
-				}
-			);
-		}
+		self.toc.replace(
+			&mut self.buf,
+			PART_INDENT,
+			match 4.min(indent) {
+				0 => b"",
+				1 => b"    ",
+				2 => b"        ",
+				3 => b"            ",
+				4 => b"                ",
+				_ => unreachable!(),
+			}
+		);
 	}
 
 	/// # Set Message.
