3c9c40fa930eba2199bd18e676d7d432dced6ec2
Josh Stoik <josh@blobfolio.com>
2020-09-16 20:24:07 -0700

    fix: non-simd methods

--- fyi_menu/src/keymaster.rs
+++ fyi_menu/src/keymaster.rs
@@ -58,101 +58,7 @@ impl KeyMaster {
 	pub const fn len(&self) -> usize { self.len }
 }
 
-#[cfg(not(feature = "simd"))]
-/// ## Safe Implementations.
-///
-/// These methods do not make use of the crate feature `simd`.
-impl KeyMaster {
-	/// # Insert.
-	///
-	/// If the key is not already stored, it will be added and `true` will be
-	/// returned, otherwise no action will be taken and `false` will be
-	/// returned.
-	///
-	/// If the maximum number of keys has already been reached, an error
-	/// message will be printed and the program will exit with a status code of
-	/// `1`.
-	pub fn insert(&mut self, key: &str, idx: usize) -> bool {
-		if self.len >= MAX_KEYS {
-			die(b"Too many options.");
-			unreachable!();
-		}
 
-		let key = hash64(key.as_bytes());
-		if self.keys[0..self.len].iter().all(|x| x.ne(&key)) {
-			self.keys[self.len] = key;
-			self.values[self.len] = idx;
-			self.len += 1;
-			true
-		}
-		else { false }
-	}
-
-	/// # Insert (Unique).
-	///
-	/// This is just like '[`KeyMaster::insert`] except that if a duplicate key
-	/// is submitted, it will print an error and exit with status code 1.
-	pub fn insert_unique(&mut self, key: &str, idx: usize) {
-		if self.len >= MAX_KEYS {
-			die(b"Too many options.");
-			unreachable!();
-		}
-
-		let key = hash64(key.as_bytes());
-		if self.keys[0..self.len].iter().any(|x| x.eq(&key)) {
-			die(b"Duplicate key.");
-			unreachable!();
-		}
-
-		self.keys[self.len] = key;
-		self.values[self.len] = idx;
-		self.len += 1;
-	}
-
-	#[must_use]
-	/// # Has Key?
-	///
-	/// Returns `true` if the key is stored, or `false` if not.
-	pub fn contains(&self, key: &str) -> bool {
-		let key = hash64(key.as_bytes());
-		self.keys[0..self.len].iter().any(|x| x.eq(&key))
-	}
-
-	#[must_use]
-	/// # Has Either of Two Keys?
-	///
-	/// This is a convenience method that checks for the existence of two keys
-	/// at once, returning `true` if either are present.
-	pub fn contains2(&self, short: &str, long: &str) -> bool {
-		let short = hash64(short.as_bytes());
-		let long = hash64(long.as_bytes());
-		self.keys[0..self.len].iter().any(|x| x.eq(&short) || x.eq(&long))
-	}
-
-	#[must_use]
-	/// # Get Key's Index.
-	///
-	/// If a key is present, return its corresponding index; if not, `None`.
-	pub fn get(&self, key: &str) -> Option<usize> {
-		let key = hash64(key.as_bytes());
-		self.keys[0..self.len].iter()
-			.position(|x| x.eq(&key))
-			.map(|x| self.values[x])
-	}
-
-	#[must_use]
-	/// # Get Either of Two Key's Index.
-	///
-	/// This is a convenience method that checks for the existence of two keys
-	/// at once, returning the first index found, if any.
-	pub fn get2(&self, short: &str, long: &str) -> Option<usize> {
-		let short = hash64(short.as_bytes());
-		let long = hash64(long.as_bytes());
-		self.keys[0..self.len].iter()
-			.position(|x| x.eq(&short) || x.eq(&long))
-			.map(|x| self.values[x])
-	}
-}
 
 #[cfg(feature = "simd")]
 /// ## SIMD Implementations.
@@ -247,7 +153,114 @@ impl KeyMaster {
 	/// This is a convenience method that checks for the existence of two keys
 	/// at once, returning the first index found, if any.
 	pub fn get2(&self, short: &str, long: &str) -> Option<usize> {
-		self.get(short).or_else(|| self.get(long))
+		let mut res = self.keys.eq(u64x8::splat(hash64(short.as_bytes()))).bitmask().trailing_zeros();
+		if res < 8 {
+			return Some(unsafe { self.values.extract_unchecked(res as usize) });
+		}
+
+		res = self.keys.eq(u64x8::splat(hash64(long.as_bytes()))).bitmask().trailing_zeros();
+		if res < 8 {
+			Some(unsafe { self.values.extract_unchecked(res as usize) })
+		}
+		else { None }
+	}
+}
+
+
+
+#[cfg(not(feature = "simd"))]
+/// ## Safe Implementations.
+///
+/// These methods do not make use of the crate feature `simd`.
+impl KeyMaster {
+	/// # Insert.
+	///
+	/// If the key is not already stored, it will be added and `true` will be
+	/// returned, otherwise no action will be taken and `false` will be
+	/// returned.
+	///
+	/// If the maximum number of keys has already been reached, an error
+	/// message will be printed and the program will exit with a status code of
+	/// `1`.
+	pub fn insert(&mut self, key: &str, idx: usize) -> bool {
+		if self.len >= MAX_KEYS {
+			die(b"Too many options.");
+			unreachable!();
+		}
+
+		let key = hash64(key.as_bytes());
+		if self.keys[0..self.len].iter().all(|x| x.ne(&key)) {
+			self.keys[self.len] = key;
+			self.values[self.len] = idx;
+			self.len += 1;
+			true
+		}
+		else { false }
+	}
+
+	/// # Insert (Unique).
+	///
+	/// This is just like '[`KeyMaster::insert`] except that if a duplicate key
+	/// is submitted, it will print an error and exit with status code 1.
+	pub fn insert_unique(&mut self, key: &str, idx: usize) {
+		if self.len >= MAX_KEYS {
+			die(b"Too many options.");
+			unreachable!();
+		}
+
+		let key = hash64(key.as_bytes());
+		if self.keys[0..self.len].iter().any(|x| x.eq(&key)) {
+			die(b"Duplicate key.");
+			unreachable!();
+		}
+
+		self.keys[self.len] = key;
+		self.values[self.len] = idx;
+		self.len += 1;
+	}
+
+	#[must_use]
+	/// # Has Key?
+	///
+	/// Returns `true` if the key is stored, or `false` if not.
+	pub fn contains(&self, key: &str) -> bool {
+		let key = hash64(key.as_bytes());
+		self.keys[0..self.len].iter().any(|x| x.eq(&key))
+	}
+
+	#[must_use]
+	/// # Has Either of Two Keys?
+	///
+	/// This is a convenience method that checks for the existence of two keys
+	/// at once, returning `true` if either are present.
+	pub fn contains2(&self, short: &str, long: &str) -> bool {
+		let short = hash64(short.as_bytes());
+		let long = hash64(long.as_bytes());
+		self.keys[0..self.len].iter().any(|x| x.eq(&short) || x.eq(&long))
+	}
+
+	#[must_use]
+	/// # Get Key's Index.
+	///
+	/// If a key is present, return its corresponding index; if not, `None`.
+	pub fn get(&self, key: &str) -> Option<usize> {
+		let key = hash64(key.as_bytes());
+		self.keys[0..self.len].iter()
+			.position(|x| x.eq(&key))
+			.map(|x| self.values[x])
+	}
+
+	#[must_use]
+	/// # Get Either of Two Key's Index.
+	///
+	/// This is a convenience method that checks for the existence of two keys
+	/// at once, returning the first index found, if any.
+	pub fn get2(&self, short: &str, long: &str) -> Option<usize> {
+		let short = hash64(short.as_bytes());
+		let long = hash64(long.as_bytes());
+		self.keys[0..self.len].iter()
+			.position(|x| x.eq(&short) || x.eq(&long))
+			.map(|x| self.values[x])
 	}
 }
 
