b5c39283d39217515d6176f39a1eefa3b44603b0
Josh Stoik <josh@blobfolio.com>
2021-01-26 22:15:05 -0800

    misc: split benches

--- fyi_witcher/benches/fw_witcher.rs
+++ fyi_witcher/benches/fw_witcher.rs
@@ -8,29 +8,31 @@ use criterion::{
 	criterion_group,
 	criterion_main,
 };
-use fyi_witcher::Witcher;
-use std::ffi::OsStr;
-use std::path::PathBuf;
 
 
 
 fn witch(c: &mut Criterion) {
-	println!("{:?}", fyi_witcher::witch(&["/usr/share"]).len());
+	// println!("{:?}", fyi_witcher::witch(&["/usr/share"]).len());
 
 	let mut group = c.benchmark_group("fyi_witcher");
 	group.sample_size(30);
 
 	group.bench_function("witch(/usr/share)", move |b| {
-		b.iter(|| {
-			let _ = black_box(fyi_witcher::witch(&["/usr/share"]));
-		})
+		b.iter_with_setup(||
+			std::iter::once("/usr/share"),
+			|i| {
+				let _ = black_box(fyi_witcher::witch(i));
+			}
+		)
 	});
 
 	group.finish();
 }
 
 fn build(c: &mut Criterion) {
-	println!("{:?}", Witcher::default().with_path("/usr/share").build().len());
+	use fyi_witcher::Witcher;
+
+	// println!("{:?}", Witcher::default().with_path("/usr/share").build().len());
 
 	let mut group = c.benchmark_group("fyi_witcher::Witcher");
 	group.sample_size(30);
@@ -48,73 +50,11 @@ fn build(c: &mut Criterion) {
 	group.finish();
 }
 
-fn regex(c: &mut Criterion) {
-	let mut group = c.benchmark_group("fyi_witcher::Witcher");
-	group.sample_size(30);
-
-	group.bench_function(r"with_regex((?i).+\.gz$).with_path(/usr/share/man).build()", move |b| {
-		b.iter(||{
-			let _ = black_box(
-				Witcher::default()
-					.with_regex(r"(?i).+\.gz$")
-					.with_path("/usr/share/man")
-					.build()
-			);
-		})
-	});
-
-	group.finish();
-}
-
-fn filter(c: &mut Criterion) {
-	let mut group = c.benchmark_group("fyi_witcher::Witcher");
-	group.sample_size(30);
-
-	fn cb(path: &PathBuf) -> bool {
-		let bytes: &[u8] = unsafe { &*(path.as_os_str() as *const OsStr as *const [u8]) };
-		bytes.len() > 3 && bytes[bytes.len()-3..].eq_ignore_ascii_case(b".gz")
-	}
-
-	group.bench_function(r"with_filter(cb).with_path(/usr/share/man).build()", move |b| {
-		b.iter(|| {
-			let _ = black_box(
-				Witcher::default()
-					.with_filter(cb)
-					.with_path("/usr/share/man")
-					.build()
-			);
-		})
-	});
-
-	group.finish();
-}
-
-fn with_ext(c: &mut Criterion) {
-	let mut group = c.benchmark_group("fyi_witcher::Witcher");
-	group.sample_size(30);
-
-	group.bench_function(r"with_ext(.jpg).with_path(/usr/share).build()", move |b| {
-		b.iter(|| {
-			let _ = black_box(
-				Witcher::default()
-					.with_ext(b".jpg")
-					.with_path("/usr/share")
-					.build()
-				);
-		});
-	});
-
-	group.finish();
-}
-
 
 
 criterion_group!(
 	benches,
 	build,
-	filter,
-	regex,
-	with_ext,
 	witch,
 );
 criterion_main!(benches);
